3f610f9e5da2793dd6ccb8dd796947ac7e706fac
Albert Wang <albertyw@mit.edu>
2014-03-01 20:51:44 -0800

    Added url that returns times in json format

--- src/serve.py
+++ src/serve.py
@@ -4,6 +4,7 @@ This file handles the serving of data
 """
 
 from flask import Flask, render_template
+import json
 
 import settings
 import data
@@ -13,14 +14,19 @@ app.debug = settings.DEBUG
 
 @app.route("/")
 def root():
-    d = data.get_routes()
     stops = []
     for stop in settings.STOPS:
         stop['times'] = data.get_times(stop['stop_id'])
         stops.append(stop)
     return render_template('home.html', stops = stops)
 
-
+@app.route("/data/times.json")
+def get_update():
+    stops = []
+    for stop in settings.STOPS:
+        stop['times'] = data.get_times(stop['stop_id'])
+        stops.append(stop)
+    return json.dumps(stops)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9001)
