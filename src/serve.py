8f015bfc2697c00923ced0bf82a01924d34d8fed
Albert Wang <albertyw@mit.edu>
2014-03-04 00:20:00 -0800

    Deleted unused ajax request handlers

--- src/serve.py
+++ src/serve.py
@@ -1,13 +1,11 @@
 """
 Transit Status
-This file handles the serving of data
 """
 
 from flask import Flask, render_template
 import json
 
 import settings
-import data
 
 app = Flask(__name__)
 app.debug = settings.DEBUG
@@ -15,14 +13,6 @@ app.debug = settings.DEBUG
 @app.route("/")
 def root():
     return render_template('home.html', stops = json.dumps(settings.STOPS))
-
-@app.route("/data/times.json")
-def get_update():
-    stops = []
-    for stop in settings.STOPS:
-        stop['times'] = data.get_times(stop['stop_id'])
-        stops.append(stop)
-    return json.dumps(stops)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9001)
