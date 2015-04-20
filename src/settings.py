67a193dcfaca07f84c131bf0a6d4abe288582cd1
Albert Wang <albertyw@mit.edu>
2015-04-20 06:13:40 +0000

    Make debug status be read from ENV

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,11 @@
+import os
+
 DEBUG = True
+if os.getenv('DEBUG', 'True').lower() == 'false':
+    DEBUG = False
+
 STOPS = [
-    {'line_name':'KT', 'stop_id':'17361', 'stop_name':'KT Inbound'},
+    {'line_name':'KT', 'stop_id':'17361', 'stop_name':'3rd & Mariposa Inbound'},
     {'line_name':'22', 'stop_id':'16657', 'stop_name':'Tennessee St & 18th St'},
 ]
 # Each dict in STOPS is:
@@ -8,7 +13,3 @@ STOPS = [
 # stop_id - id from http://proximobus.appspot.com/agencies/sf-muni/routes/12/stops.json
 # stop_name - human readable name of the stop
 
-try:
-    from settingslocal import *
-except:
-    pass
