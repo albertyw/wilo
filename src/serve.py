093e51ea520400faef4f2f64f926135786b652a3
Albert Wang <albertyw@mit.edu>
2015-05-03 20:03:30 -0700

    Add endpoint for getting today's Giants game

--- src/serve.py
+++ src/serve.py
@@ -5,6 +5,7 @@ Transit Status
 from flask import Flask, render_template
 import json
 
+import mlb_schedule
 import settings
 
 app = Flask(__name__)
@@ -14,6 +15,10 @@ app.debug = settings.DEBUG
 def root():
     wifi = {'WIFI_NAME': settings.WIFI_NAME, 'WIFI_PASSWORD': settings.WIFI_PASSWORD}
     return render_template('home.html', stops = json.dumps(settings.STOPS), wifi=wifi)
+
+@app.route("/giants_schedule")
+def giants_schedule():
+    return json.dumps(mlb_schedule.get_todays_game())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9001)
